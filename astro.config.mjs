d919a053457a0cc218de3dc22dfb7d9df002b646
Javier Mahana <javiermahana@hotmail.com>
2026-08-20 18:43:41 -0400

    Fix 2 trying to launch to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,6 @@ export default defineConfig({
         },
     },
 
-    // 1. Reemplaza 'tu-usuario' con tu nombre de usuario exacto de GitHub
-    site: 'https://JavierMahana.github.io',
-
-    // 2. Si el nombre de tu repositorio es "natalia-mahana-web", pon '/natalia-mahana-web'.
-    // NOTA: Si el repositorio se llama exactamente "tu-usuario.github.io" o usarás un dominio propio (.com, .de), NO pongas 'base' (puedes borrar esa línea).
-    base: '/WebNatiV2',
 });
 
 
